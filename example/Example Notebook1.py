741df6bfb72bc6835318be153b677e235e055532
alvaro.menezes@bjss.com <alvaro.menezes@bjss.com>
2023-02-24 14:46:37 +0000

    test

---
+++ example/Example Notebook1.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+name = "John"
+print(f"Hello {name}")
+%run ./Notebook_B
+# print(f"Welcome back {full_name}")
+
+# COMMAND ----------
+
+
---
+++ example/Notebook_B.py
@@ -0,0 +1,5 @@
+# Databricks notebook source
+print(f"Starting B")
+#full_name = f"{name} Doe"
+print(f"Ending B")
+
